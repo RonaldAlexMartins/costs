7aab4c9f52b43f8b7ba09d3f8c057ee5bacb9ccc
Ronald Martins <ronny_nascimento@hotmail.com>
2025-01-26 21:31:26 -0300

    Projeto Inicial

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import {BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+
+function App() {
+  return (
+    <Router>
+      <ul>
+        <li></li>
+        <li></li>
+        <li></li>
+      </ul>
+      <p>Footer</p>
+    </Router>
+  );
+}
+
+export default App;
