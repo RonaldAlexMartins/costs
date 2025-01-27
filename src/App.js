458602fb9efde874092b1be3b52239d008b9849d
Ronald Martins <ronny_nascimento@hotmail.com>
2025-01-26 23:18:15 -0300

    criação de componentes

--- src/App.js
+++ src/App.js
@@ -1,31 +1,35 @@
-import {BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Switch, Route, Link } from 'react-router-dom'
 import Home from './components/pages/Home'
 import Contact from './components/pages/Contact'
 import Company from './components/pages/Company'
 import NewProject from './components/pages/NewProject'
+import Container from './components/layout/Container'
 
 function App() {
   return (
     <Router>
-      <ul>
-        <li>Home</li>
-        <li>Contato</li>
-        <li></li>
-      </ul>
-      <Switch>
-        <Route exact path="/">
-          <Home/>
-        </Route>
-        <Route exact path="/company">
-          <Company />
-        </Route>
-        <Route exact path="/contact">
-          <Contact />
-        </Route>
-        <Route exact path="/newproject">
-          <NewProject/>
-        </Route>
-      </Switch>
+      <div>
+        <Link to="/">Home</Link>
+        <Link to="/contact">Contato</Link>
+        <Link to="/company">Empresa</Link>
+        <Link to="/newproject">Novo Projeto</Link>
+      </div>
+      <Container customClass="min-height">
+        <Switch>
+          <Route exact path="/">
+            <Home />
+          </Route>
+          <Route exact path="/company">
+            <Company />
+          </Route>
+          <Route exact path="/contact">
+            <Contact />
+          </Route>
+          <Route exact path="/newproject">
+            <NewProject />
+          </Route>
+        </Switch>
+      </Container>
       <p>Footer</p>
     </Router>
   );
---
+++ src/components/layout/Container.js
@@ -0,0 +1,9 @@
+import style from './Container.module.css'
+
+function Container(props) {
+    return (
+        <div className={`${style.Container} ${style[props.customClass]}`}>{props.children}</div>
+    )
+}
+
+export default Container
