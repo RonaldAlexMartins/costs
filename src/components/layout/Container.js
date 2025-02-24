05cf174d15184ded22ba4ac89599d8659baabae1
Ronald Martins <ronny_nascimento@hotmail.com>
2025-02-23 23:33:12 -0300

    Criação dos Cards de Projetos

--- src/components/layout/Container.js
+++ src/components/layout/Container.js
@@ -2,7 +2,7 @@ import style from './Container.module.css'
 
 function Container(props) {
     return (
-        <div className={`${style.Container} ${style[props.customClass]}`}>{props.children}</div>
+        <div className={`${style.container} ${style[props.customClass]}`}>{props.children}</div>
     )
 }
 
--- src/components/pages/projects.js
+++ src/components/pages/projects.js
@@ -1,11 +1,17 @@
 import { useLocation } from "react-router-dom";
 
 import Message from "../layout/Message";
-import Container from '../layout/Container'
-import styles from './Projects.module.css'
-import LinkButton from '../layout/LinkButton'
+import Container from '../layout/Container';
+import styles from './Projects.module.css';
+import LinkButton from '../layout/LinkButton';
+import ProjectCard from '../project/ProjectCard';
+import { useState, useEffect } from "react";
 
-function Projects(){
+
+function Projects() {
+
+    const [projects, setProjects] = useState([])
+
 
     const location = useLocation();
 
@@ -15,16 +21,40 @@ function Projects(){
         message = location.state.message
     }
 
+    useEffect(() => {
+        fetch('http://localhost:5000/projects', {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        })
+            .then((resp) => resp.json())
+            .then((data) => {
+                console.log("Dados Recebidos:", data)
+                setProjects(data)
+            })
+            .catch((err) => console.log(err))
+    }, [])
+
     return (
         <div className={styles.project_container}>
             <div className={styles.title_container}>
                 <h1>Meus Projetos</h1>
                 <LinkButton to="/newproject" text="Criar Projetos" />
-            </div>            
-            {location.state?.message && <Message msg={message} type="success"/>}
-            <Container customClass="start">
+            </div>
 
-                <p>Projetos.....</p>
+            {location.state?.message && <Message msg={message} type="success" />}
+            <Container customClass="start">
+                {projects.length > 0 &&
+                    projects.map((project) => (
+                        <ProjectCard
+                            key={project.id}
+                            id={project.id}
+                            name={project.name}
+                            budget={project.budget}
+                            category={project.category.name}
+                        />
+                    ))}
             </Container>
         </div>
     )
