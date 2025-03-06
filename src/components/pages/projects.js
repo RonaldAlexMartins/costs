e4d401a92964e91ce59855a912c8d336ed1e8143
Ronald Martins <ronny_nascimento@hotmail.com>
2025-03-06 00:05:39 -0300

    removendo Projetos

--- src/components/pages/projects.js
+++ src/components/pages/projects.js
@@ -13,6 +13,7 @@ function Projects() {
 
     const [projects, setProjects] = useState([])
     const [removeloading, setRemoveLoading] = useState(false)
+    const [projectMessage, setProjectMessage] = useState('')
 
     const location = useLocation();
 
@@ -40,13 +41,31 @@ function Projects() {
         }, 3000)
     }, [])
 
+    function removeProject(id) {
+
+        fetch(`http://localhost:5000/projects/${id}`, {
+            method: 'DELETE',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        })
+            .then((resp) => resp.json())
+            .then((data) => {
+                console.log("Projeto Removido:", data)
+                setProjects(projects.filter((project) => project.id !== id))
+                setProjectMessage('Projeto removido com sucesso!')
+            })
+            .catch((err) => console.log(err))
+
+    }
+
     return (
         <div className={styles.project_container}>
             <div className={styles.title_container}>
                 <h1>Meus Projetos</h1>
                 <LinkButton to="/newproject" text="Criar Projetos" />
             </div>
-
+            {projectMessage && <Message msg={projectMessage} type="success" />}
             {location.state?.message && <Message msg={message} type="success" />}
             <Container customClass="start">
                 {projects.length > 0 &&
@@ -57,10 +76,12 @@ function Projects() {
                             name={project.name}
                             budget={project.budget}
                             category={project.category.name}
+                            handleRemove={removeProject}
                         />
                     ))}
                 {!removeloading && <Loading />}
-                {removeloading && projects.length === 0 && <Message msg="Nenhum projeto cadastrado" type="info" />}
+                {removeloading && projects.length === 0 && 
+                <Message msg="Nenhum projeto cadastrado" type="info" />}
             </Container>
         </div>
     )
